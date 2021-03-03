cf970db5644066f12f894d8f7dd4e25f8eb0092b
gajen <meetgajen@yahoo.com>
2021-03-03 13:49:25 -0500

    commit tree 37a631fd5adb0989570de0be7f581ac6c450fb0c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+   
+    attendees.map do |names|
+         "Hello, my name is #{names}."
+    end
+end
+ 
+def assign_rooms(people)
+    people.map.with_index(1) do |mans, index| 
+        "Hello, #{mans}! You'll be assigned to room #{index}!"
+
+    end
+end
+
+def printer(names)
+batch_badge_creator(names).each do |badge|
+    puts badge
+end
+assign_rooms(names).each do |room|
+    puts room
+end
+end
